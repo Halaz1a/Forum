6434da9f69ef4a69ce7b0dc479990fcbb3db5a21
Tom <senkustark@gmail.com>
2025-04-25 11:30:34 +0200

    création messageModel

---
+++ lib/models/messageModel.dart
@@ -0,0 +1,125 @@
+import 'dart:convert';
+import 'package:forum/tools/secureStorage.dart';
+import 'package:http/http.dart' as http;
+import '../tools/config.dart';
+
+class Message{
+  final int id;
+  final String titre;
+  final DateTime datePoste;
+  final String contenu;
+  final int userId;
+  final String userPrenom;
+  final String userNom;
+  final int forumId;
+  final Message? parent;
+  final List<Message>? reponses;
+
+
+  Message({
+    required this.id,
+    required this.titre,
+    required this.datePoste,
+    required this.contenu,
+    required this.userId,
+    required this.userPrenom,
+    required this.userNom,
+    required this.forumId,
+    this.parent,
+    this.reponses
+  });
+
+  factory Message.fromJson(Map<String, dynamic> json) {
+    return Message(
+      id: json['id'],
+      titre: json['titre'],
+      datePoste: json['datePoste'],
+      contenu: json['contenu'],
+      userNom: json['user']['nom'],
+      userId: json['user']['id'],
+      userPrenom: json['user']['prenom'],
+      forumId: json['forum']['id'],
+      parent: json['parent'],
+      reponses: json['messages']
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    String? parentLink;
+    if(parent != null) {
+      parentLink = "/forum/api/messages/$parent.id";
+    }
+    return {
+      'id' : id,
+      'titre' : titre,
+      'datePoste' : datePoste,
+      'contenu' : contenu, 
+      'user' : "/forum/api/users/$userId",
+      'forum' : "/forum/api/forum/$forumId",
+      'parent' : parentLink
+    };
+  }
+}
+
+class MessageApi{
+
+  final storage = SecureStorage();
+
+  Future<List<Message>> allMessages() async {
+    final response = await http.get(Uri.parse('${Config.apiUrl}/messages'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+
+      List<Message> forums = data.map((json) {
+        return Message.fromJson(json);
+      }).toList();
+
+      return forums;
+
+    } else {
+      throw Exception('Failed to load forums');
+    }
+  }
+
+  Future<bool> addMessage({required String titre, required String contenu, required int userId, required int forumId, int? parentId}) async {
+    String? parentLink;
+    if (parentId != null) {
+      parentLink = "/forum/api/messages/$parentId";
+    }
+    
+    final token = await storage.readToken();
+
+    final response = await http.post(Uri.parse('${Config.apiUrl}/messages'),
+      headers: {'Authorization' : 'Bearer $token', 'Content-Type': 'application/json'},
+      body: jsonEncode({
+      'titre' : titre,
+      'datePoste' : DateTime.now(),
+      'contenu' : contenu, 
+      'user' : "/forum/api/users/$userId",
+      'forum' : "/forum/api/forum/$forumId",
+      'parent' : parentLink
+      })
+    );
+
+    if (response.statusCode == 201) {
+      return true;
+    } else {
+      throw Exception('Failed to add forum');
+    }
+  }
+
+  Future<bool> deleteForum({required int id}) async {
+    final token = await storage.readToken();
+
+    final response = await http.delete(Uri.parse('${Config.apiUrl}/messages/$id'),
+        headers: {'Authorization' : 'Bearer $token', 'Content-Type': 'application/json'}
+    );
+
+    if (response.statusCode == 204) {
+      return true;
+    } else {
+      throw Exception('Failed to delete forum');
+    }
+  }
+}
