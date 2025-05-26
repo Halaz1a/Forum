20e0b2e2ca6faeaeeb111e3ff88f77776d0d022d
Elise <elise.bonnet2005@gmail.com>
2025-05-26 17:47:44 +0200

    Publication liens API Elise et Prod

---
+++ lib/tools/config.dart
@@ -0,0 +1,9 @@
+// Données à ne pas mettre sur git
+
+class Config{
+  //API Elise Dev
+  static const String apiUrl = 'https://s4-8039.nuage-peda.fr/forum/api';
+
+  //API Prod
+  //static const String apiUrl = 'https://epsi.nuage-pedagogique.fr/forumTEP/api';
+}
